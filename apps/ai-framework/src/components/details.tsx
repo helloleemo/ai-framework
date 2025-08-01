b0f5834f0bd091dc134599789f53e3a79ed80dd6
leemo.tseng <leemo.tseng@deltaww.com>
2025-08-01 10:47:02 +0800

    feat: integrate shadcn plugin

--- apps/ai-framework/src/components/details.tsx
+++ apps/ai-framework/src/components/details.tsx
@@ -1,7 +1,10 @@
+import { Button } from './ui/button';
+
 export default function Details() {
   return (
     <div>
       <p className="text-blue-500">/Page1/Details</p>
+      <Button>Click me</Button>
     </div>
   );
 }
--- apps/ai-framework/vite.config.ts
+++ apps/ai-framework/vite.config.ts
@@ -1,8 +1,8 @@
 /// <reference types='vitest' />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(() => ({
   root: __dirname,
